de9c0cf5b4bd1eac4be395a9d5e10992bf7d7fdb
nertil <nertilpoci@hotmail.com>
2020-06-11 02:10:26 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -11,6 +11,8 @@ locals {
   lambda_content = "lambda.zip"
   dynamotable="items"
 } 
+
+//configure
  provider "aws" {
   version = "~> 2.0"
   region  = "us-east-1"
@@ -18,6 +20,7 @@ locals {
   secret_key = "LqydZtBaB7HfMd4OZxaBFutiFxI6Z2fRP9goScLt"
 }
 
+//create s3 bucket
 resource "aws_s3_bucket" "b" {
   bucket = local.bucket_name
   acl    = "private"
@@ -38,42 +41,33 @@ resource "aws_s3_bucket_object" "object" {
   etag = "${filemd5("${path.module}/${local.lambda_content}")}"
 }
  
- # IAM role which dictates what other AWS services the Lambda function
- # may access.
- resource "aws_iam_role" "lambda_exec" {
-   name = "function-${local.bucket_name}-policy"
+# lambda permissions
 
-   assume_role_policy = <<EOF
+resource "aws_iam_role" "lambda_exec" {
+  name = "myrole"
+
+  assume_role_policy = <<POLICY
 {
   "Version": "2012-10-17",
   "Statement": [
     {
-      "Sid": "",
-      "Effect": "Allow",
+      "Action": "sts:AssumeRole",
       "Principal": {
-        "Service": [
-          "lambda.amazonaws.com"
-        ]
+        "Service": "lambda.amazonaws.com"
       },
-      "Action": "sts:AssumeRole"
+      "Effect": "Allow",
+      "Sid": ""
     }
   ]
 }
- EOF
-
- }
+POLICY
+}
 
 
 resource "aws_lambda_function" "getbyidfunction" {
    function_name = "getitembyid-${local.functionrandomextension}"
-
-   # The bucket name as created earlier with "aws s3api create-bucket"
    s3_bucket = local.bucket_name
    s3_key    = local.lambda_content
-
-   # "main" is the filename within the zip file (main.js) and "handler"
-   # is the name of the property under which the handler function was
-   # exported in that file.
    handler = "src/handlers/get-by-id.getByIdHandler"
    runtime = "nodejs10.x"
 
@@ -81,50 +75,232 @@ resource "aws_lambda_function" "getbyidfunction" {
    depends_on = [
    "aws_s3_bucket_object.object"
   ]
+   environment {
+    variables = {
+      SAMPLE_TABLE = local.dynamotable
+    }
+  }
+ }
+resource "aws_lambda_function" "putitemfunction" {
+   function_name = "putitem-${local.functionrandomextension}"
+
+   s3_bucket = local.bucket_name
+   s3_key    = local.lambda_content
+   handler = "src/handlers/put-item.putItemHandler"
+   runtime = "nodejs10.x"
+
+   role = aws_iam_role.lambda_exec.arn
+   depends_on = [
+   "aws_s3_bucket_object.object"
+  ]
+   environment {
+    variables = {
+      SAMPLE_TABLE = local.dynamotable
+    }
+  }
+ }
+ 
+resource "aws_lambda_function" "getallitemsfunction" {
+   function_name = "putgetallitems-${local.functionrandomextension}"
+
+   s3_bucket = local.bucket_name
+   s3_key    = local.lambda_content
+   handler = "src/handlers/get-all-items.getAllItemsHandler"
+   runtime = "nodejs10.x"
+
+   role = aws_iam_role.lambda_exec.arn
+   depends_on = [
+   "aws_s3_bucket_object.object"
+  ]
+   environment {
+    variables = {
+      SAMPLE_TABLE = local.dynamotable
+    }
+  }
  }
 
 
-resource "aws_api_gateway_rest_api" "example" {
-  name        = "ServerlessExample"
-  description = "Terraform Serverless Application Example"
+# Create dynamo db table
+
+ resource "aws_dynamodb_table" "dynamotable" {
+  name             = local.dynamotable
+  hash_key         = "id"
+  billing_mode     = "PAY_PER_REQUEST"
+ 
+  attribute {
+    name = "id"
+    type = "S"
+  }
+  attribute {
+    name = "name"
+    type = "S"
+  }
+  global_secondary_index {
+    name               = "NameIndex"
+    hash_key           = "name"
+    write_capacity     = 10
+    read_capacity      = 10
+    projection_type    = "INCLUDE"
+    non_key_attributes = ["id"]
+  }
+
+
 }
- resource "aws_api_gateway_resource" "proxy" {
-   rest_api_id = aws_api_gateway_rest_api.example.id
-   parent_id   = aws_api_gateway_rest_api.example.root_resource_id
-   path_part   = "awslab"
+
+#lambda access to dynamodb
+
+
+
+# POLICIES
+resource "aws_iam_role_policy" "dynamodb-lambda-policy"{
+  name = "dynamodb_lambda_policy"
+  role = "${aws_iam_role.lambda_exec.id}"
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Action": [
+        "dynamodb:*"
+      ],
+      "Resource": "${aws_dynamodb_table.dynamotable.arn}"
+    }
+  ]
+}
+EOF
 }
 
-resource "aws_api_gateway_method" "proxy" {
-   rest_api_id   = aws_api_gateway_rest_api.example.id
-   resource_id   = aws_api_gateway_resource.proxy.id
-   http_method   = "ANY"
-   authorization = "NONE"
- }
- resource "aws_api_gateway_integration" "lambda" {
-   rest_api_id = aws_api_gateway_rest_api.example.id
-   resource_id = aws_api_gateway_method.proxy.resource_id
-   http_method = aws_api_gateway_method.proxy.http_method
 
-   integration_http_method = "GET"
-   type                    = "AWS_PROXY"
-   uri                     = aws_lambda_function.getbyidfunction.invoke_arn
- }
-  resource "aws_api_gateway_method" "proxy_root" {
-   rest_api_id   = aws_api_gateway_rest_api.example.id
-   resource_id   = aws_api_gateway_rest_api.example.root_resource_id
-   http_method   = "GET"
-   authorization = "NONE"
- }
 
- resource "aws_api_gateway_integration" "lambda_root" {
-   rest_api_id = aws_api_gateway_rest_api.example.id
-   resource_id = aws_api_gateway_method.proxy_root.resource_id
-   http_method = aws_api_gateway_method.proxy_root.http_method
 
-   integration_http_method = "GET"
-   type                    = "AWS_PROXY"
-   uri                     = aws_lambda_function.getbyidfunction.invoke_arn
- }
+
+ # Set permissions on the lambda function, allowing API Gateway to invoke the function
+resource "aws_lambda_permission" "allow_api_gateway-getbyidfunction" {
+  # The action this permission allows is to invoke the function
+  action = "lambda:InvokeFunction"
+
+  # The name of the lambda function to attach this permission to
+  function_name = "${aws_lambda_function.getbyidfunction.arn}"
+
+  # An optional identifier for the permission statement
+  statement_id = "AllowExecutionFromApiGateway"
+
+  # The item that is getting this lambda permission
+  principal = "apigateway.amazonaws.com"
+
+  # /*/*/* sets this permission for all stages, methods, and resource paths in API Gateway to the lambda
+  # function. - https://bit.ly/2NbT5V5
+  source_arn = "${aws_api_gateway_rest_api.api.execution_arn}/*/*/*"
+}
+ # Set permissions on the lambda function, allowing API Gateway to invoke the function
+resource "aws_lambda_permission" "allow_api_gateway-putitemfunction" {
+  # The action this permission allows is to invoke the function
+  action = "lambda:InvokeFunction"
+
+  # The name of the lambda function to attach this permission to
+  function_name = "${aws_lambda_function.putitemfunction.arn}"
+
+  # An optional identifier for the permission statement
+  statement_id = "AllowExecutionFromApiGateway"
+
+  # The item that is getting this lambda permission
+  principal = "apigateway.amazonaws.com"
+
+  # /*/*/* sets this permission for all stages, methods, and resource paths in API Gateway to the lambda
+  # function. - https://bit.ly/2NbT5V5
+  source_arn = "${aws_api_gateway_rest_api.api.execution_arn}/*/*/*"
+}
+
+ # Set permissions on the lambda function, allowing API Gateway to invoke the function
+resource "aws_lambda_permission" "allow_api_gateway-getallitemsfunction" {
+  # The action this permission allows is to invoke the function
+  action = "lambda:InvokeFunction"
+
+  # The name of the lambda function to attach this permission to
+  function_name = "${aws_lambda_function.getallitemsfunction.arn}"
+
+  # An optional identifier for the permission statement
+  statement_id = "AllowExecutionFromApiGateway"
+
+  # The item that is getting this lambda permission
+  principal = "apigateway.amazonaws.com"
+
+  # /*/*/* sets this permission for all stages, methods, and resource paths in API Gateway to the lambda
+  # function. - https://bit.ly/2NbT5V5
+  source_arn = "${aws_api_gateway_rest_api.api.execution_arn}/*/*/*"
+}
+
+# API Gateway
+resource "aws_api_gateway_rest_api" "api" {
+  name = "awslab"
+}
+
+resource "aws_api_gateway_resource" "mainapiresource" {
+  path_part   = "awslab"
+  parent_id   = "${aws_api_gateway_rest_api.api.root_resource_id}"
+  rest_api_id = "${aws_api_gateway_rest_api.api.id}"
+}
+//get all items
+//getall items method
+resource "aws_api_gateway_method" "getallmethod" {
+  rest_api_id   = "${aws_api_gateway_rest_api.api.id}"
+  resource_id   = "${aws_api_gateway_resource.mainapiresource.id}"
+  http_method   = "GET"
+  authorization = "NONE"
+}
+//getallitems integration
+resource "aws_api_gateway_integration" "getallitemsintegration" {
+  rest_api_id             = "${aws_api_gateway_rest_api.api.id}"
+  resource_id             = "${aws_api_gateway_resource.mainapiresource.id}"
+  http_method             = "${aws_api_gateway_method.getallmethod.http_method}"
+  integration_http_method = "POST"
+  type                    = "AWS_PROXY"
+  uri                     = "${aws_lambda_function.getallitemsfunction.invoke_arn}"
+}
+
+//put item method
+resource "aws_api_gateway_method" "putitemmethod" {
+  rest_api_id   = "${aws_api_gateway_rest_api.api.id}"
+  resource_id   = "${aws_api_gateway_resource.mainapiresource.id}"
+  http_method   = "POST"
+  authorization = "NONE"
+}
+//getitembyid integration
+resource "aws_api_gateway_integration" "putitemintegration" {
+  rest_api_id             = "${aws_api_gateway_rest_api.api.id}"
+  resource_id             = "${aws_api_gateway_resource.mainapiresource.id}"
+  http_method             = "${aws_api_gateway_method.putitemmethod.http_method}"
+  integration_http_method = "POST"
+  type                    = "AWS_PROXY"
+  uri                     = "${aws_lambda_function.putitemfunction.invoke_arn}"
+}
+
+
+
+//get item by id
+resource "aws_api_gateway_resource" "getbyidresource" {
+  path_part   = "{id}"
+  parent_id   = "${aws_api_gateway_resource.mainapiresource.id}"
+  rest_api_id = "${aws_api_gateway_rest_api.api.id}"
+}
+//get item by id method
+resource "aws_api_gateway_method" "getbyidmethod" {
+  rest_api_id   = "${aws_api_gateway_rest_api.api.id}"
+  resource_id   = "${aws_api_gateway_resource.getbyidresource.id}"
+  http_method   = "GET"
+  authorization = "NONE"
+}
+//getitembyid integration
+resource "aws_api_gateway_integration" "getbyideintegration" {
+  rest_api_id             = "${aws_api_gateway_rest_api.api.id}"
+  resource_id             = "${aws_api_gateway_resource.getbyidresource.id}"
+  http_method             = "${aws_api_gateway_method.getbyidmethod.http_method}"
+  integration_http_method = "POST"
+  type                    = "AWS_PROXY"
+  uri                     = "${aws_lambda_function.getbyidfunction.invoke_arn}"
+}
+
 
  output "bucket_name" {
   value = local.bucket_name
