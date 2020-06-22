442defd44fb3bf768c69e236584acd3532f56f1c
nertil <nertilpoci@hotmail.com>
2020-06-23 01:39:24 +0200

    cors

--- app/.aws-sam/build/putItemFunction/.aws-sam/build/getByIdFunction/src/handlers/get-all-items.js
+++ app/.aws-sam/build/putItemFunction/.aws-sam/build/getByIdFunction/src/handlers/get-all-items.js
@@ -28,6 +28,11 @@ exports.getAllItemsHandler = async (event) => {
 
     const response = {
         statusCode: 200,
+        headers: {
+            "Access-Control-Allow-Headers" : "Content-Type",
+            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Methods": "OPTIONS,POST,GET"
+        },
         body: JSON.stringify(Items),
     };
 
--- app/.aws-sam/build/putItemFunction/src/handlers/put-item.js
+++ app/.aws-sam/build/putItemFunction/src/handlers/put-item.js
@@ -33,6 +33,11 @@ exports.putItemHandler = async (event) => {
 
     const response = {
         statusCode: 200,
+        headers: {
+            "Access-Control-Allow-Headers" : "Content-Type",
+            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Methods": "OPTIONS,POST,GET"
+        },
         body,
     };
 
