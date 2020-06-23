6d4ec45202bdb9b1d6102df34d9db1dbff14002e
nertil <nertilpoci@hotmail.com>
2020-06-23 03:41:57 +0200

    cors

--- main.tf
+++ main.tf
@@ -241,6 +241,14 @@ resource "aws_api_gateway_resource" "mainapiresource" {
   parent_id   = "${aws_api_gateway_rest_api.api.root_resource_id}"
   rest_api_id = "${aws_api_gateway_rest_api.api.id}"
 }
+module "cors" {
+  source = "squidfunk/api-gateway-enable-cors/aws"
+  version = "0.3.1"
+
+  api_id          = "${aws_api_gateway_rest_api.api.id}"
+  api_resource_id = "${aws_api_gateway_resource.mainapiresource.id}"
+}
+
 //get all items
 //getall items method
 resource "aws_api_gateway_method" "getallmethod" {
@@ -302,81 +310,36 @@ resource "aws_api_gateway_integration" "getbyideintegration" {
   uri                     = "${aws_lambda_function.getbyidfunction.invoke_arn}"
 }
 
-resource "aws_api_gateway_method_response" "getallmethod_response_200" {
-    rest_api_id   = "${aws_api_gateway_rest_api.api.id}"
-    resource_id   = "${aws_api_gateway_resource.mainapiresource.id}"
-    http_method   = "${aws_api_gateway_method.getallmethod.http_method}"
-    status_code   = "200"
-    response_parameters = {
-        "method.response.header.Access-Control-Allow-Origin" = true
+# resource "aws_api_gateway_method_response" "getallmethod_response_200" {
+#     rest_api_id   = "${aws_api_gateway_rest_api.api.id}"
+#     resource_id   = "${aws_api_gateway_resource.mainapiresource.id}"
+#     http_method   = "${aws_api_gateway_method.getallmethod.http_method}"
+#     status_code   = "200"
+#     response_parameters = {
+#         "method.response.header.Access-Control-Allow-Origin" = true
         
-    }
-    depends_on = ["aws_api_gateway_method.getallmethod"]
-}
-resource "aws_api_gateway_method_response" "putmethod_response_200" {
-    rest_api_id   = "${aws_api_gateway_rest_api.api.id}"
-    resource_id   = "${aws_api_gateway_resource.mainapiresource.id}"
-    http_method   = "${aws_api_gateway_method.putitemmethod.http_method}"
-    status_code   = "200"
-    response_parameters = {
-        "method.response.header.Access-Control-Allow-Headers" = true,
-        "method.response.header.Access-Control-Allow-Methods" = true,
-        "method.response.header.Access-Control-Allow-Origin" = true
-    }
-    depends_on = ["aws_api_gateway_method.putitemmethod"]
-}
+#     }
+#     depends_on = ["aws_api_gateway_method.getallmethod"]
+# }
+# resource "aws_api_gateway_method_response" "putmethod_response_200" {
+#     rest_api_id   = "${aws_api_gateway_rest_api.api.id}"
+#     resource_id   = "${aws_api_gateway_resource.mainapiresource.id}"
+#     http_method   = "${aws_api_gateway_method.putitemmethod.http_method}"
+#     status_code   = "200"
+#     response_parameters = {
+#         "method.response.header.Access-Control-Allow-Headers" = true,
+#         "method.response.header.Access-Control-Allow-Methods" = true,
+#         "method.response.header.Access-Control-Allow-Origin" = true
+#     }
+#     depends_on = ["aws_api_gateway_method.putitemmethod"]
+# }
 
-#options
-
-resource "aws_api_gateway_method" "options_method" {
-    rest_api_id   = "${aws_api_gateway_rest_api.api.id}"
-    resource_id   = "${aws_api_gateway_resource.mainapiresource.id}"
-    http_method   = "OPTIONS"
-    authorization = "NONE"
-}
-resource "aws_api_gateway_method_response" "options_200" {
-    rest_api_id   = "${aws_api_gateway_rest_api.api.id}"
-    resource_id   = "${aws_api_gateway_resource.mainapiresource.id}"
-    http_method   = "${aws_api_gateway_method.options_method.http_method}"
-    status_code   = "200"
-    response_models ={
-        "application/json" = "Empty"
-    }
-    response_parameters= {
-        "method.response.header.Access-Control-Allow-Headers" = true,
-        "method.response.header.Access-Control-Allow-Methods" = true,
-        "method.response.header.Access-Control-Allow-Origin" = true
-    }
-    depends_on = ["aws_api_gateway_method.options_method"]
-}
-resource "aws_api_gateway_integration" "options_integration" {
-    rest_api_id   = "${aws_api_gateway_rest_api.api.id}"
-    resource_id   = "${aws_api_gateway_resource.mainapiresource.id}"
-    http_method   = "${aws_api_gateway_method.options_method.http_method}"
-    type          = "MOCK"
-    depends_on = ["aws_api_gateway_method.options_method"]
-}
-resource "aws_api_gateway_integration_response" "options_integration_response" {
-    rest_api_id   = "${aws_api_gateway_rest_api.api.id}"
-    resource_id   = "${aws_api_gateway_resource.mainapiresource.id}"
-    http_method   = "${aws_api_gateway_method.options_method.http_method}"
-    status_code   = "${aws_api_gateway_method_response.options_200.status_code}"
-    response_parameters = {
-        "method.response.header.Access-Control-Allow-Headers" = "'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token'",
-        "method.response.header.Access-Control-Allow-Methods" = "'GET,OPTIONS,POST,PUT'",
-        "method.response.header.Access-Control-Allow-Origin" = "'*'"
-    }
-    depends_on = ["aws_api_gateway_method_response.options_200"]
-}
-
-#-optons
 
 resource "aws_api_gateway_deployment" "lab" {
    depends_on = [
      aws_api_gateway_integration.getbyideintegration,
      aws_api_gateway_integration.getallitemsintegration,
-     aws_api_gateway_integration.putitemintegration,
-     aws_api_gateway_integration.options_integration
+     aws_api_gateway_integration.putitemintegration
      
    ]
 
